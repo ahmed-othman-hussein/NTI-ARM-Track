71acba5af58f8ea7e6ab94d028e179ebf1235502
ahmed othman <ahmed.othman.eltaweel@gmail.com>
2021-02-13 23:40:30 +0200

    MCAL

---
+++ NTI ARM Track/MCAL/Systick/STK_config.h
@@ -0,0 +1,18 @@
+/*
+ * STK_config.h
+ *
+ *  Created on: Feb 10, 2021
+ *      Author: ahmed
+ */
+
+#ifndef STK_CONFIG_H_
+#define STK_CONFIG_H_
+/*
+ * Systick Prescaller Can be :
+ *
+			STK_AHB_DIV_8
+			STK_AHB_DIV_1
+*/
+#define 		STK_PRESCALLER			STK_AHB_DIV_8
+
+#endif /* STK_CONFIG_H_ */
